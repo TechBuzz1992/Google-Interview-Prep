c47f942f6e05cc941ded13c41be6560a9849b153
TechBuzz1992 <techbuzz1992@outlook.com>
2020-06-03 21:22:24 +0530

    commit

---
+++ src/easy/BestTImeStock.java
@@ -0,0 +1,26 @@
+package easy;
+
+import java.util.*;
+
+public class BestTImeStock {
+    public static void main(String[] args) {
+
+    }
+
+    public int getMaxProfit(int[] prices) {
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                max = Math.max(max, prices[i] - min);
+            }
+
+        }
+
+        return max;
+
+    }
+
+}
