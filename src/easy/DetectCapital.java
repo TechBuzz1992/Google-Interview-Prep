8aa89bcbed5da91bbcb97f9c8a612d9a72629652
TechBuzz1992 <techbuzz1992@outlook.com>
2020-06-04 21:18:11 +0530

    commit

--- src/easy/DetectCapital.java
+++ src/easy/DetectCapital.java
@@ -2,9 +2,30 @@ package easy;
 
 public class DetectCapital {
     public static void main(String[] args) {
+        String str1 = "USA";
+        String str2 = "leetcode";
+        String str3 = "Google";
+        String str4 = "gooGle";
+
+        System.out.println(new DetectCapital().detectCapitalUse(str1));
+        System.out.println(new DetectCapital().detectCapitalUse(str2));
+        System.out.println(new DetectCapital().detectCapitalUse(str3));
+        System.out.println(new DetectCapital().detectCapitalUse(str4));
         
     }
 
-    
+    public boolean detectCapitalUse(String str){
+        int count = 0;
+        int i = 0;
+        while(i<str.length()){
+            if(Character.isUpperCase(str.charAt(i))){
+                count++;
+            }
+            i++;
+        }
+
+        return count == 0 || count == 1 && Character.isUpperCase(str.charAt(0)) || count == str.length();
+    }
+
     
 }
