dbfe2057c7d40fd7252586f3e73e0bfd0e4ebef5
TechBuzz1992 <techbuzz1992@outlook.com>
2020-06-04 14:53:42 +0530

    commit

--- src/easy/RemoveElement.java
+++ src/easy/RemoveElement.java
@@ -1,5 +1,27 @@
 package easy;
 
+import java.util.*;
+
 public class RemoveElement {
-    
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,3,4,5,6};
+        int k = 3;
+
+        System.out.println(new RemoveElement().removeInPlace(nums, k));
+
+    }
+
+    public int removeInPlace(int[] nums, int k) {
+        int index = 0;
+        for (int i : nums) {
+            if (i != k) {
+                nums[index++] = i;
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+
+        return index;
+    }
+
 }
