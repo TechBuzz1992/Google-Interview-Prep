ec3e227ce611f90f0c3281565be34b7c8c6e262e
TechBuzz1992 <techbuzz1992@outlook.com>
2020-06-04 22:28:44 +0530

    commit

--- src/easy/CountPrimes.java
+++ src/easy/CountPrimes.java
@@ -2,12 +2,38 @@ package easy;
 
 public class CountPrimes {
     public static void main(String[] args) {
-        
+        int n = 10;
+
+        System.out.println(new CountPrimes().countPrimes(n));
+
     }
 
-    public int countPrimes(int n){
-        int count = 0;
-        
+    public int countPrimes(int n) {
+        // int count = 0;
+        boolean[] primes = new boolean[n];
+        for (int i = 0; i < primes.length; i++) {
+            primes[i] = true;
+
+        }
+
+        for (int i = 2; i * i < primes.length; i++) {
+            if (primes[i]) {
+                for (int j = i; j * i < primes.length; j++) {
+                    primes[j * i] = false;
+                }
+            }
+        }
+
+        int primeCount = 0;
+        for (int i = 2; i < primes.length; i++) {
+            if (primes[i]) {
+                primeCount++;
+
+            }
+        }
+
+        return primeCount;
+
     }
-    
+
 }
