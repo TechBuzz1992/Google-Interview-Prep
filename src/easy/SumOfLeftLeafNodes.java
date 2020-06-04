7816b5deb2dbed89d0e80b44dced94cd4cd640b0
TechBuzz1992 <techbuzz1992@outlook.com>
2020-06-04 16:00:09 +0530

    commit

---
+++ src/easy/SumOfLeftLeafNodes.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.*;
+
+public class SumOfLeftLeafNodes {
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            this.data = x;
+            this.left = this.right = null;
+        }
+    }
+
+    public int getLeafNodesSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else if (root.left != null && root.left.left == null && root.left.right == null) {
+            return root.left.data + getLeafNodesSum(root.right);
+
+        } else {
+
+            return getLeafNodesSum(root.left) + getLeafNodesSum(root.right);
+        }
+    }
+
+}
