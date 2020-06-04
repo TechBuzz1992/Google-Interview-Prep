9135d233b1e7ea7ce9db1400cef92d5146c18321
TechBuzz1992 <techbuzz1992@outlook.com>
2020-06-04 12:54:45 +0530

    commit

--- src/easy/MeetingRooms.java
+++ src/easy/MeetingRooms.java
@@ -3,10 +3,38 @@ package easy;
 import java.util.*;
 
 public class MeetingRooms {
-    public static void main(String[] args) {
-        
+    static class Interval {
+        int start;
+        int end;
+
+        Interval(int x, int y) {
+            this.start = x;
+            this.end = y;
+        }
     }
 
+    public static void main(String[] args) {
 
-    
+    }
+
+    public boolean canAttendMeetings(Interval[] nums) {
+        int[] start = new int[nums.length];
+        int[] end = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            start[i] = nums[i].start;
+            end[i] = nums[i].end;
+        }
+
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (start[i + 1] < end[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
