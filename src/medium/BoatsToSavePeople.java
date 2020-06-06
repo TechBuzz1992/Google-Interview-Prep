68ea3e1ec774b58db184651bf940298b13f38f17
TechBuzz1992 <techbuzz1992@outlook.com>
2020-06-06 14:31:26 +0530

    commit

--- src/medium/BoatsToSavePeople.java
+++ src/medium/BoatsToSavePeople.java
@@ -1,10 +1,32 @@
 package medium;
 
+import java.util.*;
+
 public class BoatsToSavePeople {
     public static void main(String[] args) {
-        
+
     }
 
-    
-    
+    public int getNumOfBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int boats = 0;
+
+        int low = 0;
+        int high = people.length - 1;
+        while (low <= high) {
+            if (people[low] + people[high] <= limit) {
+                low++;
+                high--;
+            } else {
+                high--;
+            }
+
+            boats++;
+
+        }
+
+        return boats;
+
+    }
+
 }
