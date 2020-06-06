e8f737e18698cfa2b2b1db7f94da12d9ac5f3963
TechBuzz1992 <techbuzz1992@outlook.com>
2020-06-06 21:04:47 +0530

    commit

--- src/medium/BTLongestConsecutiveSubsequence.java
+++ src/medium/BTLongestConsecutiveSubsequence.java
@@ -1,10 +1,42 @@
 package medium;
 
 public class BTLongestConsecutiveSubsequence {
-    public static void main(String[] args) {
-        
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            this.val = x;
+            this.left = this.right = null;
+        }
     }
 
-    
-    
+    public static void main(String[] args) {
+
+    }
+
+    public int longestConsecutive(TreeNode root) {
+        int[] max = new int[1];
+        findLongestConsecutiveSequence(root, 0, 0, max);
+        return max[0];
+
+    }
+
+    public void findLongestConsecutiveSequence(TreeNode root, int count, int target, int[] max) {
+        if (root == null) {
+            return;
+        } else if (root.val == target) {
+            count++;
+
+        } else {
+            count = 1;
+        }
+
+        max[0] = Math.max(max[0], count);
+        findLongestConsecutiveSequence(root.left, count, root.val + 1, max);
+        findLongestConsecutiveSequence(root.right, count, root.val + 1, max);
+
+    }
+
 }
