37a16f845b65f87ae43be0b88279ed8ba7db1090
TechBuzz1992 <techbuzz1992@outlook.com>
2020-06-07 09:46:11 +0530

    commit

---
+++ src/medium/GameOfLife.java
@@ -0,0 +1,14 @@
+package medium;
+
+import java.util.*;
+
+public class GameOfLife {
+    public static void main(String[] args) {
+        
+    }
+
+    public void gameOfLife(int[][] board){
+        
+    }
+    
+}
---
+++ src/medium/WildCardMatching.java
@@ -0,0 +1,12 @@
+package medium;
+
+import java.util.*;
+
+public class WildCardMatching {
+    public static void main(String[] args) {
+        
+    }
+
+
+    
+}
