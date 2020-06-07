d17e7d8dc610cc941e5754d782d3d4d8585ee714
TechBuzz1992 <techbuzz1992@outlook.com>
2020-06-07 10:25:28 +0530

    commit

---
+++ src/hard/WildCardMatching.java
@@ -0,0 +1,16 @@
+package hard;
+
+import java.util.*;
+
+public class WildCardMatching {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isMatch(String string, String pattern) {
+        int s = string.length();
+        int p = pattern.length();
+        
+    }
+
+}
--- src/medium/GameOfLife.java
+++ src/medium/GameOfLife.java
@@ -1,14 +1,36 @@
 package medium;
 
-import java.util.*;
+//import java.util.*;
 
 public class GameOfLife {
     public static void main(String[] args) {
-        
+
     }
 
-    public void gameOfLife(int[][] board){
-        
+    public void gameOfLife(int[][] board) {
+        for (int i = 1; i < board.length - 1; i++) {
+            for (int j = 1; i < board[i].length - 1; j++) {
+                int live = 0;
+                for (int l = -1; l <= 1; l++) {
+                    for (int m = -1; m <= 1; m++) {
+                        live += board[i + l][j + m];
+                    }
+                }
+
+                live -= board[i][j];
+
+                if (board[i][j] == 1 && live < 2) {
+                    board[i][j] = 0;
+                } else if (board[i][j] == 1 && live > 3) {
+                    board[i][j] = 0;
+                } else if (board[i][j] == 0 && live == 3) {
+                    board[i][j] = 1;
+                }
+
+            }
+        }
+
+        return;
     }
-    
+
 }
